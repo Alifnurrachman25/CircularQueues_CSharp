0a0442bb371bcbea6b685140ffc44c3d8f4c4ed8
Alif Nur Rachman <alif.nur.ft21@mail.umy.ac.id>
2022-12-16 17:04:53 +0700

    create class Queues and method Queues

--- CircularQueues_CSharp/Program.cs
+++ CircularQueues_CSharp/Program.cs
@@ -1,13 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace CircularQueues_CSharp
 {
-    internal class Program
+    class Queues
     {
+        int FRONT, REAR, max = 5;
+        int[] queue_array = new int[5];
+        public Queues()
+        {
+            /*Initializing the values of the variables REAR and FRONT to -1 to indicate that the queue is initialy empty*/
+            FRONT = -1;
+            REAR = -1;
+        }
         static void Main(string[] args)
         {
         }
